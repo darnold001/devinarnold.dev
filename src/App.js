c044784029dbba440a4082996ac514092e773c84
Devin Arnold <dbarnold321@gmail.com>
2020-12-31 12:41:11 -0700

    updated router

--- src/App.js
+++ src/App.js
@@ -1,30 +1,36 @@
 import React from "react";
-import Footer from './Components/footer'
+import Footer from "./Components/footer";
 import "./App.css";
-import Navigation from './Components/navigation'
-import { CSSTransition } from 'react-transition-group'
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import Navigation from "./Components/navigation";
+import { CSSTransition } from "react-transition-group";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
 import About from "./Components/pages/about";
 import Contact from "./Components/pages/contact";
-import Welcome from './Components/pages/welcome'
+import Welcome from "./Components/pages/welcome";
+import PageNotFound from "./Components/pages/pageNotFound";
 // import ComingSoon from './Components/comingSoon'
-import Projects from './Components/pages/projects'
-
+import Projects from "./Components/pages/projects";
 
 function App() {
-  const myRef = React.createRef()
-  const [showNav, setShowNav] = React.useState(true)
+  const myRef = React.createRef();
+  const [showNav, setShowNav] = React.useState(true);
   const routes = [
-    { path: '/', name: 'Home', Component: Welcome },
-    { path: '/about', name: 'About', Component: About },
-    { path: '/contact', name: 'Contact', Component: Contact },
-    { path: '/projects', name: 'Projects', Component: Projects},
-  ]
+    { path: "/welcome", name: "Home", Component: Welcome },
+    { path: "/about", name: "About", Component: About },
+    { path: "/contact", name: "Contact", Component: Contact },
+    { path: "/projects", name: "Projects", Component: Projects },
+  ];
   const nodeRef = React.useRef(null);
   return (
     <div className="App">
-          <Router>
-          {showNav && <Navigation />}
+      <Router>
+        {showNav && <Navigation />}
+        <Switch>
           {routes.map(({ path, Component }) => (
             <Route key={path} exact path={path}>
               {({ match }) => (
@@ -37,16 +43,28 @@ function App() {
                   unmountOnExit
                 >
                   <div className="page">
-                    <Component showNav = {() => setShowNav(!showNav)}/>
+                    <Component showNav={() => setShowNav(!showNav)} />
                   </div>
                 </CSSTransition>
               )}
             </Route>
           ))}
-          </Router>
-          {showNav && <Footer />}
+          <Redirect exact from="/" to="welcome" />
+
+          <Route component={PageNotFound} />
+        </Switch>
+      </Router>
+      {showNav && <Footer />}
     </div>
   );
 }
-
+console.log(`
+██████╗  █████╗    ██████╗ ███████╗██╗   ██╗
+██╔══██╗██╔══██╗   ██╔══██╗██╔════╝██║   ██║
+██║  ██║███████║   ██║  ██║█████╗  ██║   ██║
+██║  ██║██╔══██║   ██║  ██║██╔══╝  ╚██╗ ██╔╝
+██████╔╝██║  ██║██╗██████╔╝███████╗ ╚████╔╝ 
+╚═════╝ ╚═╝  ╚═╝╚═╝╚═════╝ ╚══════╝  ╚═══╝  
+                                            
+`)
 export default App;
---
+++ src/Components/pages/pageNotFound.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function PageNotFound(props){
+    return(
+        <div className = 'pageNotFound'>
+            <p>Oops, Page Not Found</p>
+        </div>
+    )
+}
